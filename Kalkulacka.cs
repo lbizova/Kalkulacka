5ca2ba576feb0ea314acc14146a3b21e0f08888c
Lucie B <bizova.lucie@gmail.com>
2025-04-10 00:19:59 +0200

    Add Kalkulacka class with basic arithmetic operations

---
+++ Kalkulacka.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Kalkulacka
+{
+    public double Secti(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Odecitani(double a, double b)
+    {
+        return a - b;
+    }
+
+    // Později můžeme přidat Nasobeni, Deleni, nebo třeba Mocniny atd.
+}
